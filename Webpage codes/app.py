3d18349493d0c9a17ddd6dffa398f2744bbccded
Bvenedik <146375512+Bvenedik@users.noreply.github.com>
2024-05-09 22:15:53 +0200

    Update app.py

--- Webpage codes/app.py
+++ Webpage codes/app.py
@@ -1,19 +1,25 @@
 from flask import Flask, render_template, request, jsonify
 from flask_cors import CORS
-from joblib import load
+import joblib as jl
 from sklearn.preprocessing import LabelEncoder
 import numpy as np
+import pandas as pd
 
 
 app = Flask(__name__)
 CORS(app)  
 
 
-rf_model = load('models/random_forest_model.joblib')
-nb_model  = load('models/naive_bayes_model.joblib')
-svm_model = load('models/svm_model.joblib')
-rc_nb_model = load('models/rc_nb_model.joblib')
-rc_rf_model = load('models/rc_rf_model.joblib')
+rf_model = jl.load('models/random_forest_model.joblib')
+nb_model  = jl.load('models/naive_bayes_model.joblib')
+svm_model = jl.load('models/svm_model.joblib')
+
+DATA_PATH = "Training.csv"
+training_data = pd.read_csv(DATA_PATH).dropna(axis = 1)
+
+
+encoder = LabelEncoder()
+training_data["prognosis"] = encoder.fit_transform(training_data["prognosis"])
 
 
 @app.route('/page1')
@@ -27,34 +33,46 @@ def predict_model1():
         'message': 'Invalid payload',
     })
     response.status_code = 400
-
-    data = request.get_json() 
+    
+    data = request.get_json()
+    input = data['symptoms']
+    
+    
+    
     if not data or 'symptoms' not in data:
         return response
-
-    symptoms = data['symptoms']
+    
+    
+    symptoms = jl.load('models/symptoms_list.joblib')
+    #print(symptoms)
     
     symptom_index = {}
     for index, value in enumerate(symptoms):
-        symptom = " ".join([i.capitalize() for i in str(value).split("_")])
+        symptom = " ".join([i.capitalize() for i in value.split("_")])
         symptom_index[symptom] = index
-
-    encoder = LabelEncoder()
+        #print(symptom)
+        #print(symptom_index)
+        
     data_dict = {
         "symptom_index":symptom_index,
-        "predictions_classes":encoder
+        "predictions_classes":encoder.classes_
     }
 
+    print(data_dict)
+
+
     input_data = [0] * len(data_dict["symptom_index"])
-    for symptom in symptoms:
+    for symptom in input:
         index = data_dict["symptom_index"][symptom]
         input_data[index] = 1
+        
 
-    print("Input data array:", input_data) 
+    #print("Input data array:", input_data) 
     input_data = np.array(input_data).reshape(1,-1)
     
-    print("Input data shape:", input_data.shape)
-    print("Input data:", input_data)
+    #print("Input data shape:", input_data.shape)
+    #print("Input data:", input_data)
+
 
     predictions = {
         'Random Forest': data_dict["predictions_classes"][rf_model.predict(input_data)[0]],
@@ -65,38 +83,6 @@ def predict_model1():
     response.status_code = 200
     return response
 
-
-@app.route('/page2')
-def page2():
-    return render_template('recommending.html')
-
-@app.route('/predict_model2', methods=['POST'])
-
-def predict_model2():
-    print('AAAAAAAAAAAAAAAAAAAAA')
-
-    data = request.json()
-
-    disease = int(data['disease'])
-    gender = int(data['gender'])
-    age = int(data['age'])
-    
-    rf_prediction = predict_drug(rc_rf_model, disease, gender, age)
-    nb_prediction = predict_drug(rc_nb_model, disease, gender, age)
-
-    predictions = {
-    'Random Forest': rf_prediction,
-    'Naive Bayes': nb_prediction
-    }
-    response = jsonify(predictions)
-    response.status_code = 200
-    return response
-
-
-def predict_drug(model, disease, gender, age):
-    test_data = np.array([[disease, gender, age]])
-    prediction = model.predict(test_data)
-    return prediction[0]
 
 @app.after_request
 def after_request(response):
